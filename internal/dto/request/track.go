404175dd4d12e40c60e595305097c75f5b858ece
NaChOoV <fuenzalida.veas@gmail.com>
2025-10-15 11:43:05 -0300

    fix: remove omitempty from JSON tag for Alias field in CreateTrackDTO

--- internal/dto/request/track.go
+++ internal/dto/request/track.go
@@ -11,7 +11,7 @@ type CreateTrackDTO struct {
 	ChatID     string     `json:"chatId" validate:"required"`
 	ExternalID int32      `json:"externalId"`
 	Run        string     `json:"run" validate:"required"`
-	Alias      *string    `json:"alias,omitempty" validate:"omitempty,min=1,max=100"`
+	Alias      *string    `json:"alias" validate:"omitempty,min=1,max=100"`
 	FullName   string     `json:"fullName"`
 	LastEntry  *time.Time `json:"lastEntry"`
 	LastExit   *time.Time `json:"lastExit"`
